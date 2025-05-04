aaa3b6acecfb9eb639053795b408a470bda6dfd6
Maoz-Daniel <maoz2231@gmail.com>
2025-05-04 17:31:09 +0300

    delet 2

--- Stage_B/Delete/delete_2/delete_2.sql
+++ Stage_B/Delete/delete_2/delete_2.sql
@@ -3,4 +3,9 @@ WHERE
     isActive = FALSE
     AND personID NOT IN (
         SELECT DISTINCT personID FROM entryRecord
+    )
+    AND personID IN (
+        SELECT personID
+        FROM person
+        WHERE dateOfBirth > '2000-01-01'
     );
