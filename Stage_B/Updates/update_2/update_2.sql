2baeb964c6b7cb1555d226a6c0cef85e63aaca69
Maoz-Daniel <maoz2231@gmail.com>
2025-05-05 14:17:37 +0300

    end of

--- Stage_B/Updates/update_2/update_2.sql
+++
@@ -1,9 +0,0 @@
-UPDATE member
-SET membershipType = 'Expired'
-WHERE 
-    isActive = FALSE
-    AND NOT EXISTS (
-        SELECT 1
-        FROM entryRecord e
-        WHERE e.personID = member.personID
-    );
